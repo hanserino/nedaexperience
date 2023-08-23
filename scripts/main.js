6695d95b6bd195faa7fed39b81c711008b461ea9
Hans Kristian Smedsrød <eplehans@gmail.com>
2023-08-23 12:35:35 +0200

    rema 1000 and logo carousel

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,35 @@ var isTouchDevice = function () {
     );
 };
 
+let logo = document.querySelectorAll('.header__logos__logo');
+let logoActiveClass = "header__logo--active";
 
+let slideIndex = 1;
+showLogo(slideIndex);
+
+function logoCarousel(n) {
+    showLogo(slideIndex += n);
+}
+function showLogo(n) {
+    let i;
+    if (document.querySelector(".header__logos__logo")) {
+        if (n > logo.length) {
+            slideIndex = 1
+        };
+        if (n < 1) {
+            slideIndex = logo.length
+        };
+        for (i = 0; i < logo.length; i++) {
+            logo[i].style.display = "none";
+        };
+
+        logo[slideIndex-1].style.display = "block";
+    }
+}
+
+setInterval(function() {
+    logoCarousel(1)
+}, 2000);
 
 function init() {
     var touchClass = isTouchDevice() ? "touch" : "no-touch";
